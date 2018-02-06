11c387b103dd5e64084c133f95d4fa2300e53900
Evan Schwander <evan.schwander@gmail.com>
2018-02-05 20:19:48 -0800

    Finishing thought

--- Parser.py
+++ Parser.py
@@ -4,9 +4,8 @@ from Sheet import Sheet
 class Gene_Stats:
     def __init__(self, name):
         self.name = name
-        self.b = []
-        self.m = []
-        self.e = []
+        self.values = {"BEG":[], "MID":[], "END":[], "WHOLE":[]}
+
 
 input_path = "./input"
 input_files = []
@@ -23,6 +22,10 @@ for file in input_files:
     sheet = Sheet(file, PEAK="AGGREGATE")
     for row in sheet.rows:
         gene_name = row.get_value("#GENE")
+        int_type = row.get_value("INTTYPE")
+        odds_ratio = row.get_value("ODDSRATIO")
         if gene_name not in genes:
             genes[gene_name] = Gene_Stats(gene_name)
-        if
+        genes[gene_name].values[int_type].append(odds_ratio)
+
+print("End?")
