879a4d70214f4207b7663529b1364cf680881d04
Evan Schwander <evan.schwander@gmail.com>
2018-02-05 20:15:34 -0800

    Finished reading sheet

--- Parser.py
+++ Parser.py
@@ -1,8 +1,28 @@
-from os import listdir
-from os.path import isfile, join
+import os
+from Sheet import Sheet
+
+class Gene_Stats:
+    def __init__(self, name):
+        self.name = name
+        self.b = []
+        self.m = []
+        self.e = []
 
 input_path = "./input"
+input_files = []
+for root, dirs, files in os.walk(input_path):
+    for f in files:
+        if f.endswith(".csv"):
+            input_files.append(root + "/" + f)
 
-input_files = [f for f in listdir(input_path) if isfile(join(input_path, f))]
+print(input_files)
+
+genes = {}
 
 for file in input_files:
+    sheet = Sheet(file, PEAK="AGGREGATE")
+    for row in sheet.rows:
+        gene_name = row.get_value("#GENE")
+        if gene_name not in genes:
+            genes[gene_name] = Gene_Stats(gene_name)
+        if
---
+++ Sheet.py
@@ -0,0 +1,39 @@
+import csv
+
+class Sheet:
+    class Row:
+        def __init__(self, sheet, row):
+            self.row = row
+            self.sheet = sheet
+
+        def get_value(self, column_name):
+            index = self.sheet.headers[column_name]
+            return self.row[index]
+
+    def __init__(self, file_path, **filter):
+        self.filter = filter
+        self.rows = []
+        self.headers = {}
+        with open(file_path) as file:
+            reader = csv.reader(file)
+            for i, row in enumerate(reader):
+                if i == 0:
+                    self.set_headers(row)
+                else:
+                    self.filter_row(row)
+
+    def filter_row(self, row):
+        temp_row = self.Row(self, row)
+        add_row = True
+        for k in self.filter:
+            if temp_row.get_value(k) != self.filter[k]:
+                add_row = False
+        if add_row:
+            self.rows.append(temp_row)
+
+    def set_headers(self, first_row):
+        for i, c in enumerate(first_row):
+            self.headers[c] = i
+
+    def get_row(self, row_num):
+        return self.rows[row_num]
